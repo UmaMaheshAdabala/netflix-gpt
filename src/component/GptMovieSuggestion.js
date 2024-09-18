c4799359d34ef949748be3341dd398ece04e8f11
Uma Mahesh <adabala.um@gmail.com>
2024-09-18 15:12:51 +0530

    GPT Search created using openai API

--- src/component/GptMovieSuggestion.js
+++ src/component/GptMovieSuggestion.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import MovieList from "./MovieList"
 
 const GptMovieSuggestion = () => {
+  const {movieNames, movieResults} =  useSelector(store => store.gpt);
+  if(!movieNames) return null;
   return (
-    <div>
-      GptMovieSuggestion
+    <div className='p-4 m-4 '>
+        {movieNames.map((movieName, index) => <MovieList key={movieName} title={movieName} movies={movieResults[index]}/>)}
+        
     </div>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const API_OPTIONS = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzAyMTE4N2YwNjEwNWIyOGViZWZkNWRlYTE3MjcxMiIsIm5iZiI6MTcyNTQ2Mjg4Mi43MDU1ODcsInN1YiI6IjY2ZDg3M2IxYjU0MmQ0N2Y4MDQ3NDk2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3A3V1htnEoqOw2O4-fC_BJG-1it46QTGi6tJj1JeFyU",
+      "Bearer "+process.env.REACT_APP_TMDB_KEY,
   },
 };
 
@@ -15,5 +15,5 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
-export const OPENAI_KEY =
-  "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";
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+  
